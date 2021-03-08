b3d7f3fceb98fa6a8ec3c344679f50cae267d057
Alberto <75929385+mha97137@users.noreply.github.com>
2021-03-08 21:34:19 +0100

    Add files via upload

---
+++ src/model/player.py
@@ -0,0 +1,7 @@
+class Player:
+    def __init__(self, nom, prenom, naissance, sexe, classement):
+        self.nom = nom
+        self.prenom = prenom
+        self.naissance = naissance
+        self.sexe = sexe
+        self.classement = classement
